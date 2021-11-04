42e4618fb345c732f1daa8d14326ef1842710a43
et9719 <ella-t@live.co.uk>
2021-11-04 19:53:03 +0000

    Add welcome function to run.py

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from words import words
+
+def welcome():
+    """
+    Show welcome message and ask user if they would like to see instructions.
+    """
+    print("{:-^45}".format("HANGMAN"))
+    print("\n")
+    print("{:^45}".format("Welcome to Hangman!\n"))
+    print("{:^45}".format("Before we start, would you like to see "))
+    print("{:^45}".format("the instructions or are you good to go?\n"))
+    print("{:^45}".format("Please type 1 to see the instructions,"))
+    
+    see_instructions = input("{:^45}".format("or 2 for good to go!: \n"))
+    
+    if see_instructions == "1":
+        print("Instructions")
+    else:
+        print("Start game")
+
+
+# def get_word():
+#     """
+#     Get word from words.py words list for user to guess.
+#     """
+#     word = random.choice(words)
+#     return word.upper()
+    
+welcome()
