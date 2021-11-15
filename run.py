08db84ed6c28c9401cb55dc448e91774834561fd
et9719 <ella-t@live.co.uk>
2021-11-15 23:37:55 +0000

    Add colors to the text

--- run.py
+++ run.py
@@ -10,6 +10,7 @@ def title():
     """
     Print hangman title.
     """
+    print("\033[1;36;40m")
     print("HANGMAN".center(80, "-"))
     print("\n")
 
@@ -28,9 +29,9 @@ def welcome():
     see_instructions = input("Please type 1 to see the instructions,or 2 for\
  good to go!:\n")
     # Make sure users input is valid.
-    if see_instructions != "1" and see_instructions != "2":
-        see_instructions = input("\nInvalid input, Please type 1 to see the\
- instructions, or 2 to skip them and \nstart the game:\n")
+    while see_instructions != "1" and see_instructions != "2":
+        see_instructions = input("\n\033[1;31;40mInvalid input, Please type 1 to see the\
+ instructions, or 2 to skip them and start the game:\n")
     # Take user to relevent page.
     if see_instructions == "1":
         instructions()
@@ -45,19 +46,17 @@ def instructions():
     # clear terminal
     os.system("cls" if os.name == "nt" else "clear")
     # print instructions
-    print("How to play:\n\nTo play hangman, all you need to do is guess the word\
- one letter at a time.\n\n1. Type a letter of your choice and hit \
- enter.\n2. If your guess is correct the letter will show within \
- the hidden word.\n3. If your guess is incorrect a section of the hangman \
- picture will appear.\n4. Keep guessing until either you guess the\
- correct word or you run out of tries\n")
+    print("\033[1;36;40mHow to play:\n\nTo play hangman, all you need to do is guess the word\
+ one letter at a time.\n\n1. Type a letter of your choice and hit enter.\n2. If\
+ your guess is correct the letter will show within the hidden word.\n3. If your\
+ guess is incorrect a section of the hangman picture will appear.\n4. Keep guessing\
+ until either you guess the correct word or you run out of tries.\n")
     # Ask user if they are ready to play.
     print("Are you ready to play?")
     ready = input("Please type 1 for yes and 2 for no:\n")
     # Make sure users input is valid.
-    if ready != "1" and ready != "2":
-        ready = input("Invalid input, if you are ready press\
- 1 if not press 2:\n")
+    while ready != "1" and ready != "2":
+        ready = input("\n\033[1;31;40mInvalid input, if you are ready press 1 if not press 2:\n")
     # Take user to relevent page.
     if ready == "1":
         play()
@@ -69,12 +68,10 @@ def play_again():
     """
     Ask user if they would like to play the game again.
     """
-    play_again_q = input("Would you like to play again?\nType 1\
- for Yes or 2 for No:\n")
+    play_again_q = input("\033[1;36;40mWould you like to play again?\nType 1 for Yes or 2 for No:\n")
     # Make sure users input is valid.
-    if play_again_q != "1" and play_again_q != "2":
-        play_again_q = input("Invalid input, if you want to play\
- again press 1 if not press 2:\n")
+    while play_again_q != "1" and play_again_q != "2":
+        play_again_q = input("\n\033[1;31;40mInvalid input, if you want to play again press 1 if not press 2:\n")
     # Take user to relevent page.
     if play_again_q == "1":
         play()
@@ -99,6 +96,7 @@ def play():
     # write here ~~~~~~~~~~~~~~~~~~~~~~
     while tries > 0 and guessed is False:
         reveal_word = [letter if letter in guessed_letters else "_" for letter in word]
+        print("\033[1;36;40m")
         print(" ".join(reveal_word))
         print("\n")
         # Ask user to enter a letter.
@@ -110,16 +108,16 @@ def play():
         if len(users_guess) == 1 and users_guess.isalpha():
             if users_guess in guessed_letters:
                 title()
-                print("You already guessed ", users_guess, "\n")
+                print("\033[1;31;40mYou already guessed ", users_guess, "\n")
                 # print letters user has guessed so far.
-                print("You have used these letters so far:")
+                print("\033[1;36;40mYou have used these letters so far:")
                 print(" ".join(guessed_letters))
                 print("\n")
             elif users_guess not in word:
                 title()
-                print(users_guess, " is not in the word, try again.")
+                print("\033[1;31;40m", users_guess, " is not in the word, try again.")
                 tries -= 1
-                print("Tries left = ", tries)
+                print("\033[1;36;40mTries left = ", tries)
                 print("\n")
                 guessed_letters.append(users_guess)
                 print("You have used these letters so far:")
@@ -127,17 +125,17 @@ def play():
                 print("\n")
             else:
                 title()
-                print("Correct guess!\n")
+                print("\033[1;32;40mCorrect guess!\n")
                 guessed_letters.append(users_guess)
                 # print letters user has guessed so far.
-                print("You have used these letters so far:")
+                print("\033[1;36;40mYou have used these letters so far:")
                 print(" ".join(guessed_letters))
                 print("\n")
                 if "_" not in reveal_word:
                     guessed = True
         else:
             title()
-            print("Invalid guess, letters only.\n")
+            print("\n\033[1;31;40mInvalid guess, letters only.\n")
     if guessed is True:
         # clear terminal
         os.system("cls" if os.name == "nt" else "clear")
@@ -149,7 +147,7 @@ def play():
         # clear terminal and print title
         os.system("cls" if os.name == "nt" else "clear")
         title()
-        print("Your out of Tries :(")
+        print("\033[1;31;40mYour out of Tries :(")
         print("The word was", word, "\n")
         play_again()
 
