a7937296ccff5c046237995e683fd59cdbfc1ae0
et9719 <ella-t@live.co.uk>
2021-11-15 17:25:48 +0000

    Add play again function

--- run.py
+++ run.py
@@ -6,6 +6,14 @@ import random
 from words import words
 
 
+def title():
+    """
+    Print hangman title.
+    """
+    print("HANGMAN".center(80, "-"))
+    print("\n")
+
+
 def welcome():
     """
     Show welcome message and ask user if they would like to see instructions.
@@ -13,8 +21,7 @@ def welcome():
     # clear terminal
     os.system("cls" if os.name == "nt" else "clear")
     # Welcome message.
-    print("HANGMAN".center(80, "-"))
-    print("\n")
+    title()
     print("Welcome to Hangman!\n\nBefore we start, would you like to see the\
  instructions or are you good to go?")
     # Ask user to choose if they want to see instructions or not.
@@ -55,6 +62,24 @@ def instructions():
     else:
         welcome()
 
+
+def play_again():
+    """
+    Ask user if they would like to play the game again.
+    """
+    play_again_q = input("Would you like to play again?\nType 1 for Yes or 2 for No:\n")
+    # Make sure users input is valid.
+    if play_again_q != "1" and play_again_q != "2":
+        play_again_q = input("Invalid input, if you want to play again press 1 if not press 2:\n")
+    # Take user to relevent page.
+    if play_again_q == "1":
+        play()
+    else:
+        os.system("cls" if os.name == "nt" else "clear")
+        title()
+        print("Thanks for playing, goodBye!")
+
+
 def play():
     """
     Get word from words.py, ask user to guess a letter and compare results.
@@ -64,11 +89,9 @@ def play():
     tries = 6
     guessed = False
     guessed_letters = []
-    # clear terminal
+    # clear terminal and print title
     os.system("cls" if os.name == "nt" else "clear")
-    # print title
-    print("HANGMAN".center(80, "-"))
-    print("\n")
+    title()
     # write here ~~~~~~~~~~~~~~~~~~~~~~
     while tries > 0 and guessed is False:
         reveal_word = [letter if letter in guessed_letters else "_" for letter in word]
@@ -82,16 +105,14 @@ def play():
         # write here.
         if len(users_guess) == 1 and users_guess.isalpha():
             if users_guess in guessed_letters:
-                print("HANGMAN".center(80, "-"))
-                print("\n")
+                title()
                 print("You already guessed ", users_guess, "\n")
                 # print letters user has guessed so far.
                 print("You have used these letters so far:")
                 print(" ".join(guessed_letters))
                 print("\n")
             elif users_guess not in word:
-                print("HANGMAN".center(80, "-"))
-                print("\n")
+                title()
                 print(users_guess, " is not in the word, try again.")
                 tries -= 1
                 print("Tries left = ", tries)
@@ -101,63 +122,32 @@ def play():
                 print(" ".join(guessed_letters))
                 print("\n")
             else:
-                print("HANGMAN".center(80, "-"))
-                print("\n")
+                title()
                 print("Correct guess!\n")
                 guessed_letters.append(users_guess)
-                if "_" not in reveal_word:
-                    guessed = True
                 # print letters user has guessed so far.
                 print("You have used these letters so far:")
                 print(" ".join(guessed_letters))
                 print("\n")
+                if "_" not in reveal_word:
+                    guessed = True
         else:
-            print("HANGMAN".center(80, "-"))
-            print("\n")
+            title()
             print("Invalid guess, letters only.\n")
-    if tries == 0:
-        # clear terminal
-        os.system("cls" if os.name == "nt" else "clear")
-        # print title
-        print("HANGMAN".center(80, "-"))
-        print("\n")
-        print("Your out of Tries :(")
-        print("The word was", word, "\n")
-        play_again = input("Would you like to play again?\nType 1 for Yes or 2 for No:\n").upper()
-        # Make sure users input is valid.
-        if play_again != "1" and play_again != "2":
-            play_again = input("Invalid input, if you want to play again press 1 if not press 2:\n")
-        # Take user to relevent page.
-        if play_again == "1":
-            play()
-        else:
-            # clear terminal
-            os.system("cls" if os.name == "nt" else "clear")
-            # print title
-            print("HANGMAN".center(80, "-"))
-            print("\n")
-            print("Thanks for playing, goodBye!")
     if guessed is True:
         # clear terminal
         os.system("cls" if os.name == "nt" else "clear")
         # print title
-        print("HANGMAN".center(80, "-"))
-        print("\n")
+        title()
         print("Congratualtions!\nYou guessed the word, You win!\n")
-        play_again = input("Would you like to play again?\nType 1 for Yes or 2 for No:\n").upper()
-        # Make sure users input is valid.
-        if play_again != "1" and play_again != "2":
-            play_again = input("Invalid input, if you want to play again press 1 if not press 2:\n")
-        # Take user to relevent page.
-        if play_again == "1":
-            play()
-        else:
-            # clear terminal
-            os.system("cls" if os.name == "nt" else "clear")
-            # print title
-            print("HANGMAN".center(80, "-"))
-            print("\n")
-            print("Thanks for playing, goodBye!")
+        play_again()
+    else:
+        # clear terminal and print title
+        os.system("cls" if os.name == "nt" else "clear")
+        title()
+        print("Your out of Tries :(")
+        print("The word was", word, "\n")
+        play_again()
 
 
 welcome()
