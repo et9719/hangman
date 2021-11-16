cb5abf2b43247daafe9cabb9405850b2d71563a3
et9719 <ella-t@live.co.uk>
2021-11-16 17:25:12 +0000

    define word_letters and use while len(word_letters) > 0 to end loop when all letters have been guessed

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ def title():
     """
     Print hangman title.
     """
-    print("\033[1;36m")
+    print("\033[1;34m")
     print("HANGMAN".center(80, "-"))
     print("\n")
 
@@ -23,7 +23,7 @@ def welcome():
     os.system("cls" if os.name == "nt" else "clear")
     # Welcome message.
     title()
-    print("\033[1;36mWelcome to Hangman!\n\nBefore we start, would you like to see the\
+    print("\033[1;34mWelcome to Hangman!\n\nBefore we start, would you like to see the\
  instructions or are you good to go?")
     # Ask user to choose if they want to see instructions or not.
     see_instructions = input("Please type 1 to see the instructions, or 2 for\
@@ -47,7 +47,7 @@ def instructions():
     os.system("cls" if os.name == "nt" else "clear")
     title()
     # print instructions
-    print("\033[1;36mHow to play:\n\nTo play hangman, all you need to\
+    print("\033[1;34mHow to play:\n\nTo play hangman, all you need to\
  do is guess the word one letter at a time.\n\n1. Type a letter of your\
  choice and hit enter.\n2. If your guess is correct the letter will show\
  within the hidden word.\n3. If your guess is incorrect a section of the\
@@ -71,7 +71,7 @@ def play_again():
     """
     Ask user if they would like to play the game again.
     """
-    play_again_q = input("\033[1;36mWould you like to play\
+    play_again_q = input("\033[1;34mWould you like to play\
  again?\nType 1 for Yes or 2 for No:\n")
     # Make sure users input is valid.
     while play_again_q != "1" and play_again_q != "2":
@@ -92,15 +92,16 @@ def play():
     """
     word = random.choice(words)
     tries = 6
-    guessed = False
+    # guessed = False
+    word_letters = set(word)
     guessed_letters = []
     # Clear terminal and print title
     os.system("cls" if os.name == "nt" else "clear")
     title()
     # Compare users guess to word.
-    while tries > 0 and guessed is False:
+    while tries > 0 and len(word_letters) > 0:
         reveal_word = [letter if letter in guessed_letters else "_" for letter in word]
-        print("\033[1;36m")
+        print("\033[1;34m")
         print(hangman_img(tries))
         print("\n")
         print(" ".join(reveal_word))
@@ -116,7 +117,7 @@ def play():
             if users_guess in guessed_letters:
                 title()
                 print("\033[1;31mYou already guessed ", users_guess, "\n")
-                print("\033[1;36mYou have used these letters so far:")
+                print("\033[1;34mYou have used these letters so far:")
                 print(" ".join(guessed_letters))
             # If the users guess is not in the word:
             elif users_guess not in word:
@@ -124,7 +125,7 @@ def play():
                 print("\033[1;31m", users_guess, " is not in\
  the word, try again.")
                 tries -= 1
-                print("\033[1;36mTries left = ", tries)
+                print("\033[1;34mTries left = ", tries)
                 guessed_letters.append(users_guess)
                 print("You have used these letters so far:")
                 print(" ".join(guessed_letters))
@@ -133,31 +134,36 @@ def play():
                 title()
                 print("\033[1;32mCorrect guess!\n")
                 guessed_letters.append(users_guess)
-                print("\033[1;36mYou have used these letters so far:")
+                print("\033[1;34mYou have used these letters so far:")
                 print(" ".join(guessed_letters))
-                if "_" not in reveal_word:     # Problem with changing guessed to true.
-                    guessed = True
+                if users_guess in word_letters:
+                    word_letters.remove(users_guess)
         # If users input is not valid:
         else:
             title()
             print("\n\033[1;31mInvalid guess, letters only.\n")
-    # If the user wins and gets the whole word:
-    if guessed is True:
-        os.system("cls" if os.name == "nt" else "clear")
-        title()
-        print("Congratulations!\nYou guessed the word, You win!\n")
-        play_again()
-    # If the user runs out of tries:
-    else:
+
+    if tries == 0:
+        # If the user runs out of tries:
         os.system("cls" if os.name == "nt" else "clear")
         title()
         print(hangman_img(tries))
         print("\033[1;31mYour out of Tries :(")
         print("The word was", word, "\n")
         play_again()
+    else:
+        # If the user wins and gets the whole word:
+        os.system("cls" if os.name == "nt" else "clear")
+        title()
+        print("\033[1;32mCongratulations!\nYou \
+guessed the word", word, "You win!\n")
+        play_again()
 
 
 def hangman_img(tries):
+    """
+    Drawing of the hangman image.
+    """
     stages = [
         """
             --------
